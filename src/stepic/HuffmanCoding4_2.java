a5abea3d9c38fc99bce69959812c521531b9166e
skolesnikov <sysspb\skolesnikov>
2019-09-01 18:59:27 +0300

    Huffman coding, add generated method

--- src/stepic/HuffmanCoding4_2.java
+++ src/stepic/HuffmanCoding4_2.java
@@ -18,8 +18,8 @@ import java.util.TreeSet;
  * a: 0
  * 0
  * Sample Input 2:
+ * <p>
  * abacabad
- * zzzzaaazzzaattttrufffklllllq
  * Sample Output 2:
  * 4 14
  * a: 0
@@ -28,12 +28,13 @@ import java.util.TreeSet;
  * d: 111
  * 01001100100111
  */
+//zzzzaaazzzaattttrufffklllllq
 public class HuffmanCoding4_2 {
   private Map<Character, Integer> table = new TreeMap<>();
-  private Map<String, Integer> codeTable = new TreeMap<>();
-  private TreeMap<Integer, TreeSet<String>> sortByValue = new TreeMap<>();
+  private Map<Character, String> dictionary = new TreeMap<>();
+  private TreeMap<Integer, TreeSet<String>> sortedByValue = new TreeMap<>();
   private int numOfLeters;
-  private int lengthOfEncodedString;
+
 
   public static void main(String[] args) {
     HuffmanCoding4_2 cod = new HuffmanCoding4_2();
@@ -41,58 +42,93 @@ public class HuffmanCoding4_2 {
     Scanner in = new Scanner(System.in);
     String s = in.next();
 
-//    System.out.println(cod.numOfLeters + " " + cod.lengthOfEncodedString);
-//    cod.codeTable.forEach((key, value) -> System.out.println(key + ": " + value));
-    System.out.println(cod.encode(s));
+    String encodedStr = cod.encode(s);
+    int lengthOfEncodedString = encodedStr.length();
+    System.out.println(cod.dictionary.size() + " " + lengthOfEncodedString);
+    cod.dictionary.forEach((key, value) -> System.out.println(key + ": " + value));
+    System.out.println(encodedStr);
+
   }
 
-  private String encode(String s) {
+  public String encode(String s) {
     for (int i = 0; i < s.length(); i++) {
       table.merge(s.charAt(i), 1, Integer::sum);
     }
     numOfLeters = table.size();
-    table.forEach((key, value) -> sortByValue.computeIfAbsent(value, k -> new TreeSet<>()).add(key.toString()));
-//    table.forEach((key, value) -> System.out.println(key + ": " + value));
-    sortByValue.forEach((key, value) -> System.out.println(key + ": " + value));
+    table.forEach((key, value) -> sortedByValue.computeIfAbsent(value, k -> new TreeSet<>()).add(key.toString()));
+//    sortedByValue.forEach((key, value) -> System.out.println(key + ": " + value));
 
     StringBuilder sb = new StringBuilder();
-    while (sortByValue.size() > 2) {
-      Map.Entry<Integer, TreeSet<String>> entry = sortByValue.firstEntry();
-      TreeSet<String> val = entry.getValue();
-      Integer key = entry.getKey();
-      if (val.size() == 1) {
-        // get the lowest value of the entry
-        sb.append(val.first());
-        // remove entry because it's empty now
-        sortByValue.remove(key);
-        // get the second lowest entry
-        entry = sortByValue.firstEntry();
-        val = entry.getValue();
-        sb.append(val.pollFirst());
-        Integer newKey = key + entry.getKey();
-        if (val.isEmpty())
-          sortByValue.remove(entry.getKey());
-//        TreeSet<String> newValue = new TreeSet<>();
-//        newValue.add(sb.toString());
-        sortByValue.computeIfAbsent(newKey, k -> new TreeSet<>()).add(sb.toString());
-        sb.setLength(0);
-      } else if (val.size() > 1) {
-        sb.append(val.pollFirst());
-        sb.append(val.pollFirst());
-        if (val.isEmpty())
-          sortByValue.remove(key);
-//        TreeSet<String> newValue = new TreeSet<>();
-//        newValue.add(sb.toString());
-//        sortByValue.put(key * 2, newValue);
-        sortByValue.computeIfAbsent(key * 2, k -> new TreeSet<>()).add(sb.toString());
-        sb.setLength(0);
-      }
+    while (true) {
+      Map.Entry<Integer, TreeSet<String>> entry;
+      if (sortedByValue.size() == 2) {
+        if (sortedByValue.firstEntry().getValue().size() > 2) {
+          entry = sortedByValue.firstEntry();
+        } else if (sortedByValue.lastEntry().getValue().size() > 2) {
+          entry = sortedByValue.lastEntry();
+        } else
+          break;
+      }else
+        entry = sortedByValue.firstEntry();
+        TreeSet<String> val = entry.getValue();
+        Integer key = entry.getKey();
+        if (val.size() == 1) {
+          // get the lowest value of the entry
+          sb.append(val.first());
+          // remove entry because it's empty now
+          sortedByValue.remove(key);
+          // get the second lowest entry
+          entry = sortedByValue.firstEntry();
+          val = entry.getValue();
+          sb.append(val.pollFirst());
+          Integer newKey = key + entry.getKey();
+          if (val.isEmpty())
+            sortedByValue.remove(entry.getKey());
+          sortedByValue.computeIfAbsent(newKey, k -> new TreeSet<>()).add(sb.toString());
+          sb.setLength(0);
+        } else if (val.size() > 1) {
+          sb.append(val.pollFirst());
+          sb.append(val.pollFirst());
+          sortedByValue.computeIfAbsent(key * 2, k -> new TreeSet<>()).add(sb.toString());
+          if (val.isEmpty()) {
+            sortedByValue.remove(key);
+          }
+          sb.setLength(0);
+        }
+
     }
-    sortByValue.forEach((key, value) -> System.out.println(key + ": " + value));
-    sb.append(sortByValue.pollFirstEntry().getValue().first());
-    sb.append(sortByValue.pollFirstEntry().getValue().first());
-    System.out.println(sb.toString());
-    return "";
+    sortedByValue.forEach((key, value) -> System.out.println(key + ": " + value));
+    generateDictionary(sortedByValue.pollFirstEntry().getValue().first(), 0);
+    generateDictionary(sortedByValue.pollFirstEntry().getValue().first(), 1);
+
+//    System.out.println(sb.toString());
+    return generateEncodedString(s);
+  }
+
+  private void generateDictionary(String s, int prefix) {
+    int p = prefix ^ 1;
+    int j = 0;
+    StringBuilder sb = new StringBuilder();
+    sb.append(prefix);
+    String str = s;
+    while (true) {
+      if (str.length() == 1) {
+        dictionary.put(str.charAt(0), sb.toString());
+        break;
+      }
+      dictionary.put(str.charAt(0), sb.append(p).toString());
+      sb.deleteCharAt(++j);
+      sb.append(prefix);
+      str = str.substring(1);
+    }
+  }
+
+  private String generateEncodedString(String s) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < s.length(); i++) {
+      sb.append(dictionary.get(s.charAt(i)));
+    }
+    return sb.toString();
   }
 }
 
