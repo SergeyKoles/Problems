13dc858e1e108db073ccd85fcbee502dc64e51bb
skolesnikov <sysspb\skolesnikov>
2019-09-11 10:17:57 +0300

    priority queue fix

--- src/stepic/PriorityQueue4_3.java
+++ src/stepic/PriorityQueue4_3.java
@@ -1,5 +1,18 @@
 package stepic;
 
+
+/**
+   Insert
+   200
+   Insert
+   10
+   Insert
+   5
+   Insert
+   500
+   ExtractMax
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -7,47 +20,50 @@ import java.util.Scanner;
 public class PriorityQueue4_3 {
 
   private List<Integer> a = new ArrayList<>();
-
-  private int n = a.size();
+  private static final String INSERT = "Insert";
+  private int n = 0;
 
   public static void main(String[] args) {
 
     PriorityQueue4_3 pq = new PriorityQueue4_3();
+    pq.a.add(Integer.MAX_VALUE);
 
     Scanner in = new Scanner(System.in);
     int m = in.nextInt();
     String oper;
-    while (m-->0){
+    while (m-- > 0) {
       oper = in.next();
-      pq.insert(Integer.parseInt(oper));
+      if (oper.startsWith(INSERT)) {
+        pq.insert(in.nextInt());
+      } else {
+        System.out.println(pq.extractMax());
+      }
     }
-
-    pq.a.forEach(e-> System.out.print(e + ", "));
   }
 
   private void insert(int v) {
     n++;
-    a.add(v);
-    siftUp(n-1);
+    a.add(n, v);
+    siftUp(n);
   }
 
-  private int exctractMax() {
-    int max = a.get(0);
-    a.add(0, a.get(n));
+  private int extractMax() {
+    int e = a.get(n);
+    int max = a.remove(1);
+    a.add(1, e);
     n--;
-    siftDown(0);
+    siftDown(1);
     return max;
   }
 
   private void siftDown(int i) {
-
     int j;
     while ((2 * i) <= n) {
       j = i;
-      if (a.get(2 * i) < a.get(j)) {
+      if (a.get(2 * i) > a.get(j)) {
         j = 2 * j;
       }
-      if ((2 * i + 1) <= n && (a.get(2 * i + 1) < a.get(j))) {
+      if ((2 * i + 1) <= n && (a.get(2 * i + 1) > a.get(j))) {
         j = 2 * i + 1;
       }
       if (j == i) {
@@ -57,47 +73,28 @@ public class PriorityQueue4_3 {
         i = j;
       }
     }
-
-    /*    int j;
-    while ((2 * i) <= n) {
-      j = i;
-      if (a[2 * i] < a[j]) {
-        j = 2 * j;
-      }
-      if ((2 * i + 1) <= n && (a[2 * i + 1] < a[j])) {
-        j = 2 * i + 1;
-      }
-      if (j == i) {
-        break;
-      } else {
-        exch(i, j);
-        i = j;
-      }
-    }*/
   }
 
   private void siftUp(int i) {
-
-    while (i > 0 && a.get(i / 2) > a.get(i)) {
+    while (i > 1 && a.get(i / 2) < a.get(i)) {
       exch(i);
       i = i / 2;
     }
-
-    /*    while (i > 1 && a[i / 2] > a[i]) {
-      exch(i);
-      i = i / 2;
-    }*/
   }
 
   private void exch(int i) {
-    int tmp = a.get(i);
+    int tmp = a.remove(i);
     a.add(i, a.get(i / 2));
+    a.remove(i / 2);
     a.add(i / 2, tmp);
   }
 
   private void exch(int i, int j) {
     int tmp = a.get(i);
-    a.add(i, a.get(j));
+    int e = a.get(j);
+    a.remove(i);
+    a.add(i, e);
+    a.remove(j);
     a.add(j, tmp);
   }
 }
