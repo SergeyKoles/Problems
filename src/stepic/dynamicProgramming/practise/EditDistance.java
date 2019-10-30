9046d125f9eecc12f75560ecdd13436bbc47d154
skoles <chimbosan@yandex.ru>
2019-10-30 22:39:01 +0300

    edit distance with memory restriction, but without restoring result

--- src/stepic/dynamicProgramming/practise/EditDistance.java
+++ src/stepic/dynamicProgramming/practise/EditDistance.java
@@ -6,99 +6,41 @@ public class EditDistance {
   private static String first;
   private static String second;
 
-  private static int[][] d;
-  private static boolean[][] calculated;
+  private static int[] cur;
+  private static int[] prev;
+
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     first = scanner.next();
     second = scanner.next();
-    d = new int[first.length() + 1][second.length() + 1];
-//    for recursion
-//    calculated = new boolean[first.length() + 1][second.length() + 1];
-
+    cur = new int[second.length() + 1];
+    prev = new int[second.length() + 1];
     System.out.println(editDistance());
   }
 
   private static int editDistance() {
 
-    boolean[][] moveFirst = new boolean[first.length() + 1][second.length() + 1];
-    boolean[][] moveSecond = new boolean[first.length() + 1][second.length() + 1];
-
     for (int n = 0; n <= first.length(); n++) {
       for (int m = 0; m <= second.length(); m++) {
-        if (n == 0 && m == 0) d[n][m] = 0;
+        if (n == 0 && m == 0) cur[m] = 0;
         else if (n == 0) {
-          moveSecond[n][m] = true;
-          d[n][m] = m;
+          cur[m] = m;
         } else if (m == 0) {
-          moveFirst[n][m] = true;
-          d[n][m] = n;
+          cur[m] = n;
         } else {
-          int res1 = d[n][m - 1] + 1;
-          int res2 = d[n - 1][m] + 1;
-          int res3 = d[n - 1][m - 1] + (first.charAt(n - 1) == second.charAt(m - 1) ? 0 : 1);
-          int result;
-          if (res1 <= res2 && res1 <= res3) {
-            moveSecond[n][m] = true;
-            result=res1;
-          } else {
-            if (res2 <= res3) {
-              moveFirst[n][m] = true;
-              result=res2;
-            } else {
-              moveFirst[n][m] = true;
-              moveSecond[n][m] = true;
-              result=res3;
-            }
-          }
-          d[n][m] = result;
+          int res1 = cur[m - 1] + 1;
+          int res2 = prev[m] + 1;
+          int res3 = prev[m - 1] + (first.charAt(n - 1) == second.charAt(m - 1) ? 0 : 1);
+          int result = Math.min(Math.min(res1, res2), res3);
+          cur[m] = result;
         }
       }
+//      System.arraycopy(cur, 0, prev, 0, second.length() + 1);
+      int[] tmp = cur;
+      cur = prev;
+      prev = tmp;
     }
-    int n = first.length();
-    int m = second.length();
-
-    StringBuilder firstLine = new StringBuilder();
-    StringBuilder secondLine = new StringBuilder();
-
-    while (n > 0 || m > 0) {
-      boolean mf = moveFirst[n][m];
-      boolean ms = moveSecond[n][m];
-      if (mf) {
-        firstLine.append(first.charAt(n - 1));
-        n--;
-      } else {
-        firstLine.append("-");
-      }
-      if (ms) {
-        secondLine.append(second.charAt(m - 1));
-        m--;
-      } else {
-        secondLine.append("-");
-      }
-    }
-    firstLine.reverse();
-    secondLine.reverse();
-    System.out.println(firstLine);
-    System.out.println(secondLine);
-    return d[first.length()][second.length()];
-  }
-
-  private static int editDistanceRecursion(int n, int m) {
-    //first[0..n-1], second[0..m-1]
-    if (n == 0 && m == 0) return 0;
-    if (n == 0) return m;
-    if (m == 0) return n;
-    if (calculated[n][m]) {
-      return d[n][m];
-    }
-    int res1 = editDistanceRecursion(n, m - 1) + 1;
-    int res2 = editDistanceRecursion(n - 1, m) + 1;
-    int res3 = editDistanceRecursion(n - 1, m - 1) + (first.charAt(n - 1) == second.charAt(m - 1) ? 0 : 1);
-    int result = Math.min(Math.min(res1, res2), res3);
-    calculated[n][m] = true;
-    d[n][m] = result;
-    return result;
+    return prev[second.length()];
   }
 }
