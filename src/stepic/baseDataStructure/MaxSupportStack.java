a0674c36ffff67b7e28c543c74de8628c1368196
skolesnikov <sysspb\skolesnikov>
2019-11-12 13:58:57 +0300

    Maximum Support Stack approved

--- src/stepic/baseDataStructure/MaxSupportStack.java
+++ src/stepic/baseDataStructure/MaxSupportStack.java
@@ -8,40 +8,58 @@ import java.util.StringTokenizer;
 
 public class MaxSupportStack {
   private Stack<Integer> stack = new Stack<>();
+  private Stack<Integer> auxiliary = new Stack<>();
 
   public static void main(String[] args) {
     try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      int q = Integer.parseInt(st.nextToken());
-      MaxSupportStack maxSupportStack = new MaxSupportStack();
-      for (int i = 0; i < q; i++) {
-        st = new StringTokenizer(br.readLine());
-        switch (st.toString()) {
-          case "pop":
-            maxSupportStack.pop();
-            break;
-          case "push":
-            maxSupportStack.push(Integer.parseInt(st.nextToken()));
-            break;
-          case "max":
-            System.out.println(maxSupportStack.max());
-            break;
-        }
+      int n = Integer.parseInt(br.readLine());
+      String[] q = new String[n];
+      for (int i = 0; i < n; i++) {
+        q[i] = br.readLine();
       }
+      run(q, n);
     } catch (IOException e) {
       System.out.println("-------- Oops!!! --------");
     }
   }
 
+  private static void run(String[] q, int n) {
+    MaxSupportStack maxSupportStack = new MaxSupportStack();
+    StringTokenizer st;
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(q[i]);
+
+      switch (st.nextToken()) {
+        case "pop":
+          maxSupportStack.pop();
+          break;
+        case "push":
+          maxSupportStack.push(Integer.parseInt(st.nextToken()));
+          break;
+        case "max":
+          System.out.println(maxSupportStack.max());
+          break;
+      }
+    }
+  }
+
   private void pop() {
     stack.pop();
+    auxiliary.pop();
   }
 
   private void push(int n) {
     stack.push(n);
+    if (auxiliary.isEmpty()) {
+      auxiliary.push(n);
+    } else {
+      int max = auxiliary.peek();
+      if (n < max) auxiliary.push(max);
+      else auxiliary.push(n);
+    }
   }
 
   private int max() {
-    return 0;
+    return auxiliary.peek();
   }
 }
