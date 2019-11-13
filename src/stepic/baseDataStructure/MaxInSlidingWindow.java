27cb2f6278b9f27b1a15c90cbfd38ea649d9d712
skolesnikov <sysspb\skolesnikov>
2019-11-13 16:02:52 +0300

    Maximum in a sliding window Failed test #14.

--- src/stepic/baseDataStructure/MaxInSlidingWindow.java
+++ src/stepic/baseDataStructure/MaxInSlidingWindow.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class MaxInSlidingWindow {
   public static void main(String[] args) {
@@ -15,11 +16,22 @@ public class MaxInSlidingWindow {
         A[i] = Integer.parseInt(st.nextToken());
       }
       int m = Integer.parseInt(br.readLine());
+      new MaxInSlidingWindow().run(n, m, A);
     } catch (IOException e) {
       System.out.println("-------- Oops!!! --------");
     }
   }
 
   private void run(int n, int m, int[] A) {
+    TreeSet<Integer> wind = new TreeSet<>();
+    for (int i = 0; i < m; i++) {
+      wind.add(A[i]);
+    }
+    System.out.print(wind.last() + " ");
+    for (int i = m, j = 0; i < n; i++, j++) {
+      wind.remove(A[j]);
+      wind.add(A[i]);
+      System.out.print(wind.last() + " ");
+    }
   }
 }
