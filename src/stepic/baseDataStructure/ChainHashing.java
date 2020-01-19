59c2455ee6a4bbc561b820436a9121416bf43249
skoles <chimbosan@yandex.ru>
2020-01-19 08:45:08 +0300

    ChainHashing solved

--- src/stepic/baseDataStructure/ChainHashing.java
+++ src/stepic/baseDataStructure/ChainHashing.java
@@ -3,7 +3,7 @@ package stepic.baseDataStructure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class ChainHashing {
 
@@ -17,18 +17,19 @@ public class ChainHashing {
   private static int x = 263;
   private static int maxStringLength = 15;
 
-  private static String[] table;
+  private static Map<Integer, LinkedList<String>> table;
   private static int tableSize;
   private static long[] auxiliaryArray;
 
 
   public static void main(String[] args) {
+//    testInput(args);
     try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
       int m = Integer.parseInt(br.readLine());
       int n = Integer.parseInt(br.readLine());
       StringTokenizer st;
       tableSize = m;
-      table = new String[m];
+      table = new HashMap<>();
       auxiliaryArray = getAuxiliaryArray();
       for (int i = 0; i < n; i++) {
         st = new StringTokenizer(br.readLine());
@@ -39,6 +40,23 @@ public class ChainHashing {
     } catch (IOException e) {
       System.out.println("-------- Oops!!! --------");
     }
+  }
+
+  private static void testInput(String[] args) {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      String s = st.nextToken();
+      int m = Integer.parseInt(st.nextToken());
+      tableSize = m;
+      auxiliaryArray = getAuxiliaryArray();
+      System.out.println(testHash(s));
+    } catch (IOException e) {
+      System.out.println("-------- Oops!!! --------");
+    }
+  }
+
+  private static int testHash(String s) {
+    return (int) getHash(s);
   }
 
   private static void process(String req, String str) {
@@ -60,25 +78,30 @@ public class ChainHashing {
 
   private static void delete(String str) {
     int index = (int) getHash(str);
-    String chain = table[index];
-    if (chain != null && chain.contains(str)) {
-      chain = chain.replace(str, "");
-      table[index] = chain.replace("  ", " ");
+    LinkedList<String> values = table.get(index);
+    if (values != null) {
+      values.remove(str);
     }
   }
 
   private static String check(int index) {
-    String chain = table[index];
-    if (chain == null || "".equals(chain)) {
-      return "";
+    LinkedList<String> values = table.get(index);
+    if (values != null && !values.isEmpty()) {
+      return listToString(values);
     }
-    return chain;
+    return "";
+  }
+
+  private static String listToString(List<String> strs) {
+    if (strs != null && !strs.isEmpty())
+      return String.join(" ", strs);
+    else return "";
   }
 
   private static void find(String str) {
     int index = (int) getHash(str);
-    String chain = table[index];
-    if (chain == null || !chain.contains(str))
+    LinkedList<String> values = table.get(index);
+    if (values == null || !values.contains(str))
       System.out.println(NO);
     else
       System.out.println(YES);
@@ -86,17 +109,16 @@ public class ChainHashing {
 
   private static void add(String str) {
     int index = (int) getHash(str);
-    String chain = check(index);
-    if (chain.equals("")) {
-      table[index] = str;
-    } else
-      table[index] = str + " " + chain;
+    LinkedList<String> values = table.computeIfAbsent(index, k -> new LinkedList<>());
+    if (!values.contains(str)) {
+      values.addFirst(str);
+    }
   }
 
   private static long getHash(String str) {
     char[] chars = str.toCharArray();
     long sum = 0;
-    long m = table.length;
+    long m = tableSize;
     for (int i = 0; i < chars.length; i++) {
       sum += ((chars[i] % MOD) * (auxiliaryArray[i] % MOD)) % MOD;
     }
@@ -124,58 +146,3 @@ public class ChainHashing {
       return binPow(base, --pow) * base % MOD;
   }
 }
-
-/*
-const long long MOD = 1e9 + 7;
-
-//base ^ p
-long long bin_pow(long long base, long long p) {
-    if (p == 1) {
-        return base;    //Выход из рекурсии.
-    }
-
-    if (p % 2 == 0) {
-        long long t = bin_pow(base, p / 2);
-        return t * t % MOD;
-    } else {
-        return bin_pow(base, p - 1) * base % MOD;
-    }
-}
- */
-
-/*
-5
-12
-add world
-add HellO
-check 4
-find World
-find world
-del world
-check 4
-del HellO
-add luck
-add GooD
-check 2
-del good
-
- */
-
-/*
-[0] => 1
-[1] => 263
-[2] => 69169
-[3] => 18191447
-[4] => 784350533
-[5] => 284188737
-[6] => 741637313
-[7] => 50611954
-[8] => 310943811
-[9] => 778221726
-[10] => 672312510
-[11] => 818188898
-[12] => 183678669
-[13] => 307489611
-[14] => 869767133
-[15] => 748754383
- */
