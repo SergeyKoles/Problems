bbeb1b85920d077fc023d937afdba912f6ffdefb
skoles <chimbosan@yandex.ru>
2020-03-16 07:28:43 +0300

    RequestSumOnSegment - add test class and testData.txt

---
+++ src/stepic/baseDataStructure/test/TestRequestSumOnSegment.java
@@ -0,0 +1,40 @@
+package stepic.baseDataStructure.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestRequestSumOnSegment {
+
+  private static ArrayList<Long> list = new ArrayList<>();
+
+  public static void add(long value) {
+    if (!list.contains(value)) {
+      list.add(value);
+      Collections.sort(list);
+    }
+  }
+
+  public static void delete(long value) {
+    list.remove(value);
+  }
+
+  public static String find(long value) {
+    if (list.contains(value))
+      return "Found";
+    return "Not found";
+  }
+
+  public static long sum(long left, long right) {
+    long sum = 0;
+    for (Long i : list) {
+      if (i >= left) {
+        if (i <= right) {
+          sum += i;
+        } else {
+          return sum;
+        }
+      }
+    }
+    return sum;
+  }
+}
